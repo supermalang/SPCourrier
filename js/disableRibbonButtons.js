6de7a84e6173c71b97950be9f3b581eef3be4c3d
Elhadji Malang DIEDHIOU <supermalang@outlook.com>
2017-11-21 16:15:19 +0000

    Commentaire du fichier disableRibbonButtons.js

--- js/disableRibbonButtons.js
+++ js/disableRibbonButtons.js
@@ -1,54 +1,63 @@
 /**
  * Désactive les boutons d'actions personnalisés selon les informations du courrier
  * Exigences fontionnelles 
- *    - Le destinataire du courrier peut (Terminer, Affecter, Annoter, Transmettre)
- *    - Les personnes en copie peuvent (Affecter, Annoter, Transmettre)
+ *    - Le destinataire du courrier peut (Terminer, Assigner, Annoter, Transmettre)
+ *    - Les personnes en copie peuvent (Assigner, Annoter, Transmettre)
  *    - Les autres peuvent (Afficher le courrier seulement)
  */
 function disableRibbonButtons(){
   var ctx = new SP.ClientContext.get_current();
   var web = ctx.get_web();
   
-  // On récupère l'ID de la liste et l'ID de l'élément de liste Actif
+  /** Récupération de l'ID de la liste Active et de l'ID de l'élément de liste actif */
   var listId = _spPageContextInfo.pageListId;
   var itemId = parseInt(GetUrlKeyValue('ID'));
   
-  // Utilisateur connecté
+  /** Utilisateur Actif */
   this.currentUser = web.get_currentUser();
 
-  // On récupère le courrier (l'élément de liste)
+  /** Récupération du courrier (l'élément de liste) */
   this.list = web.get_lists().getById(listId);
   this.listItem = list.getItemById(itemId);
 
-  // Actions personnalisées des utilisateurs
+  /** Actions persnnalisées utilisateur (Boutons du ruban) */
   this.userCustomActions = list.get_userCustomActions();
 
   ctx.load(this.currentUser);
   ctx.load(this.listItem);
   ctx.load(this.userCustomActions);
   ctx.executeQueryAsync(Function.createDelegate(this, this.onSuccess), Function.createDelegate(this, this.onFail));
-  }
+}
 
-  function onSuccess(sender, args) {
-    // Nom du destinataire du courrier
+function onSuccess(sender, args) {
+    /** Destinataire du courrier */
     destinataire = this.listItem.get_item('DestinataireCourrierEntrant').$2e_1;
 
     /** Etat du courrier  */
     etatCourrier = this.listItem.get_item('EtatCourrier');
 
-    // Nom de l'utilsateur Actif
+    /** Nom de l'utilsateur Actif */
     utilisateurConnecte = this.currentUser.get_title();
-    
+
+    /** Liste des actions personnalisées */
     var customActionEnumerator = userCustomActions.getEnumerator();
+
+    /** Parcours la liste des actions personnalisées */
     while (customActionEnumerator.moveNext()) {
+        /** Action personnalisée parcourur */
         var userCustomAction = customActionEnumerator.get_current();
+        /** Titre de l'action personnalisée */
         customActionTitle = userCustomAction.get_title();
         
-        console.log(customActionTitle);
         switch(customActionTitle) {
             case 'Terminer':
+                /**
+                 * L'action 'Terminer' peut être exécutée uniquement si les conditions suivantes sont respectées :
+                 *  - L'utilisateur actif est le propriétaire du courrier
+                 *  - Le courrier n'est pas dans l'état 'Terminé'
+                 * Si les conditions ne sont pas respectées alors l'action est désactivée
+                 */
                 if (destinataire != utilisateurConnecte || etatCourrier=="Terminé"){
-                    // On désactive le bouton Terminer
                     $(".ms-cui-ctl-largelabel:contains('"+customActionTitle+"')").parent().click(function(e){
                         e.stopImmediatePropagation();
                         $(this).addClass("ms-cui-row ms-cui-disabled");
@@ -59,20 +68,32 @@ function disableRibbonButtons(){
             break;
 
             case 'Assigner':
-            if (destinataire != utilisateurConnecte || etatCourrier=="Terminé"){
-                    // On désactive le bouton Assigner
-                    $(".ms-cui-ctl-largelabel:contains('"+customActionTitle+"')").parent().click(function(e){
-                        e.stopImmediatePropagation();
-                        $(this).addClass("ms-cui-row ms-cui-disabled");
-                        return false;
-                    });
-                    $(".ms-cui-ctl-largelabel:contains('"+customActionTitle+"')").parent().addClass("ms-cui-row ms-cui-disabled");
+                /**
+                 * L'action 'Assigner' peut être exécutée uniquement si les conditions suivantes sont respectées :
+                 *  - L'utilisateur actif est le propriétaire du courrier
+                 *  - L'utilisateur actif est en copie du courrier
+                 *  - Le courrier n'est pas dans l'état 'Terminé'
+                 * Si les conditions ne sont pas respectées alors l'action est désactivée
+                 */
+                if (destinataire != utilisateurConnecte || etatCourrier=="Terminé"){
+                        $(".ms-cui-ctl-largelabel:contains('"+customActionTitle+"')").parent().click(function(e){
+                            e.stopImmediatePropagation();
+                            $(this).addClass("ms-cui-row ms-cui-disabled");
+                            return false;
+                        });
+                        $(".ms-cui-ctl-largelabel:contains('"+customActionTitle+"')").parent().addClass("ms-cui-row ms-cui-disabled");
                 }
             break;
 
             case 'Annoter':
+                /**
+                 * L'action 'Annoter' peut être exécutée uniquement si les conditions suivantes sont respectées :
+                 *  - L'utilisateur actif est le propriétaire du courrier
+                 *  - L'utilisateur actif est en copie du courrier
+                 *  - Le courrier n'est pas dans l'état 'Terminé'
+                 * Si les conditions ne sont pas respectées alors l'action est désactivée
+                 */
                 if (destinataire != utilisateurConnecte || etatCourrier=="Terminé"){
-                    // On désactive le bouton Annoter
                     $(".ms-cui-ctl-largelabel:contains('"+customActionTitle+"')").parent().click(function(e){
                         e.stopImmediatePropagation();
                         $(this).addClass("ms-cui-row ms-cui-disabled");
@@ -83,8 +104,14 @@ function disableRibbonButtons(){
             break;
 
             case 'Transmettre':
-                if (destinataire != utilisateurConnecte || etatCourrier=="Transmettre"){
-                    // On désactive le bouton Transmettre
+                /**
+                 * L'action 'Transmettre' peut être exécutée uniquement si les conditions suivantes sont respectées :
+                 *  - L'utilisateur actif est le propriétaire du courrier
+                 *  - L'utilisateur actif est en copie du courrier
+                 *  - Le courrier n'est pas dans l'état 'Terminé'
+                 * Si les conditions ne sont pas respectées alors l'action est désactivée
+                 */
+                if (destinataire != utilisateurConnecte || etatCourrier=="Terminé"){
                     $(".ms-cui-ctl-largelabel:contains('"+customActionTitle+"')").parent().click(function(e){
                         e.stopImmediatePropagation();
                         $(this).addClass("ms-cui-row ms-cui-disabled");
@@ -98,7 +125,7 @@ function disableRibbonButtons(){
                 break;
         }
     }
-  }
+}
 function onFail(sender, args) { console.log(args.get_message()); }
 //ExecuteOrDelayUntilScriptLoaded(disableRibbonButtons, "sp.ribbon.js");
 _spBodyOnLoadFunctionNames.push("disableRibbonButtons");   
