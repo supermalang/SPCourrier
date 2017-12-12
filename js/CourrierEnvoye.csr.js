2e0b6f6cd6da6207ae9c97d5e679695bfa1b30c7
Elhadji Malang DIEDHIOU <supermalang@outlook.com>
2017-12-12 14:06:43 +0000

    Ajout de commentaires

--- js/CourrierEnvoye.csr.js
+++ js/CourrierEnvoye.csr.js
@@ -1,4 +1,12 @@
+/**
+ * Ce script CSR (Client Side Rendering) permet d'améliorer l'affichage de la vue des courriers
+ * L'objet du courrier sera transformé en lien cliquable. Au clic, on ouvre la page d'affichage du courrier (DispForm.aspx)
+ */
 (function () {
+    /**
+     * Renvoie le lien de la page d'affichage (DispForm.aspx) du courrier
+     * @param {*} renderCtx Contexte de la vue
+     */
     function renderDisplayFormLink(renderCtx) {
         var item = renderCtx.CurrentItem;
         var displayFormUrl = renderCtx.displayFormUrl + '&ID=' + item.ID;
@@ -6,8 +14,11 @@
         return String.format('<a href="{0}" class="ms-listlink" onfocus="OnLink(this)" onclick="DisplayItem(event, \'{0}\');return false;">{1}</a>', displayFormUrl, item.ObjetCourrier)
     }
 
+    /** Personnalise la liste des courriers en affichage de couleur de fond alterné */
     function postRender(ctx){
+        /** Nombre de lignes (Nombre de courriers) */
         var rows = ctx.ListData.Row;
+        /** Pour alterner la couleur de fond sur les lignes de la liste de courriers */
         for (var i=0;i<rows.length;i++)
         {
             if((i%2)==0)
@@ -19,14 +30,21 @@
         }
     }
 
+    /** Efface l'affichage par défaut de la vue SharePoint et nous permet d'utiliser notre affichage */
     function overrideTemplate()
     {
+        /** Contexte de la vue */
         var ctxView = {};
+        /** Templates de la vue */
         ctxView.Templates = {};
+        /** Champs du template */
         ctxView.Templates.Fields = {
+            /** Pour le champ 'ObjetCourrier' nous personnalisons le rendu à l'aide de la fonction renderDisplayFormLink(), qui y ajoutera un lien */
             'ObjetCourrier' : { 'View': renderDisplayFormLink }
         };
+        /** Après l'exécution du rendu personnalisé de la vue, on appelle la fonction postRender() */
         ctxView.OnPostRender = postRender;
+        /** On signale à SharePoint de prendre en compte le rendu de notre vue 'ctxView' */
         SPClientTemplates.TemplateManager.RegisterTemplateOverrides(ctxView);
     } 
     ExecuteOrDelayUntilScriptLoaded(overrideTemplate, 'clienttemplates.js');
